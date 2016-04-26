4dd0edaccc627c2ec436c1ca1037e615d35afc46
ale.co1989@gmail.com <Pot_Anglon89>
2016-04-26 12:27:15 +0200

    Test add method with two number

--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -15,4 +15,12 @@ public class StringCalculatorTest {
         assertEquals(2, calculator.add("2"));
         assertEquals(3, calculator.add("3"));
     }
+
+    @Test
+    public void addTwoNumberIsSumOfNumbers() throws Exception {
+        StringCalculator calculator = new StringCalculator();
+        assertEquals(3, calculator.add("1,2"));
+        assertEquals(7, calculator.add("3,4"));
+        assertEquals(2, calculator.add("0,2"));
+    }
 }
