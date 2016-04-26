bd7cfa12611d8638d89877852fdb37eee96ba17f
ale.co1989@gmail.com <Pot_Anglon89>
2016-04-26 14:15:57 +0200

    Test add method with unknown amount of number

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -6,17 +6,14 @@ public class StringCalculator {
         int sum = 0;
         String[] parts = numbers.split(",");
 
-        if (parts.length >= 1)
-            sum += stringToInt(parts[0]);
-
-        if (parts.length >= 2)
-            sum += stringToInt(parts[1]);
+        for (String part: parts)
+            sum += stringToInt(part);
 
         return sum;
     }
 
     private int stringToInt(String string) {
-        if( string == "")
+        if (string.equals(""))
             return 0;
         return Integer.parseInt(string);
     }
